ce4b99cc51588ec1abec542fcec7dd9ba2014d76
Sasha Fedotova <80479418+FEDAlexandra@users.noreply.github.com>
2021-12-30 19:38:09 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import RGBChanger from './components/RGBChanger';
+import FibonacciMemo from './components/FibonacciMemo';
+import FibonacciCallback from './components/FibonacciCallback';
+
+function App() {
+  return (
+    <>
+      <RGBChanger />
+      <FibonacciMemo />
+      <FibonacciCallback />
+    </>
+  );
+}
+
+export default App;
